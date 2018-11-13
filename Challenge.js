8b69ec0afa5bca5467c592486172b1834f4c94fb
josieelordi <josieelordi@gmail.com>
2018-11-13 10:53:38 -0500

    challenge is separate and we have two

--- Challenge.js
+++ Challenge.js
@@ -14,15 +14,53 @@ function Timer({ interval }) {
 
 export default class Challenge extends React.Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			start: 0,
+			now: 0,
+			name : props.name
+		};
+	}
+
+	start = () => {
+		const now = new Date().getTime();
+		this.setState({
+			start: now,
+			now
+		});
+		this.timer = setInterval(() => {
+			this.setState({ now: new Date().getTime() });
+		}, 100);
+	};
+
 	render() {
 		const { now, start } = this.state;
 		const timer = now - start;
 		return (
 			<View style={styles.container}>
-				<Text>{name}</Text>
+				<Text style={styles.text}>{this.state.name}</Text>
 				<Timer interval={timer} />
-				<Button title={name} color="#A1EDCE" onPress={this.start} />
+				<Button title="reset" color="#A1EDCE" onPress={this.start} />
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#07263B",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	text: {
+		color: "#A1EDCE",
+		fontSize: 35
+	},
+	timer: {
+		color: "#ffffff",
+		fontSize: 40
+	}
+});
--- Home.js
+++ Home.js
@@ -1,50 +1,24 @@
 import React from "react";
 import { StyleSheet, Text, View, Button, AlertIOS } from "react-native";
 import moment from "moment";
-import Challenge from "./Challenge";
-
-function Timer({ interval }) {
-	const duration = moment.duration(interval);
-	return (
-		<Text style={styles.timer}>
-			{duration.days()} days, {duration.minutes()} minutes,{" "}
-			{duration.seconds()} seconds{" "}
-		</Text>
-	);
-}
-
-function Challange({ name }) {
-	return (
-		<View>
-			<Timer interval={timer} />
-			<Button title={name} color="#A1EDCE" onPress={this.start} />
-		</View>
-	);
-}
+import Challenge from "./Challenge.js";
 
 export default class Home extends React.Component {
 	static navigationOptions = { title: "Home" };
+	
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			start: 0,
-			now: 0
+			numChallanges: 0,
 		};
 	}
 
-	start = () => {
-		const now = new Date().getTime();
-		this.setState({
-			start: now,
-			now
-		});
-		this.timer = setInterval(() => {
-			this.setState({ now: new Date().getTime() });
-		}, 100);
-	};
 	addNewChallange = () => {
-		AlertIOS.prompt("Hello Fam", null, text => console.log("yo: " + text));
+		let name;
+		AlertIOS.prompt("Hello Fam", null, text => name = text);
+
+		this.state.numChallanges = this.state.numChallanges + 1;
 	};
 
 	render() {
@@ -52,7 +26,8 @@ export default class Home extends React.Component {
 		const timer = now - start;
 		return (
 			<View style={styles.container}>
-				<Challange name="Charlie's Fishbowls" />
+				<Challenge name="Charlie's Fishbowls" />
+				<Challenge name="Ricks Mind Probe" />
 				<Button
 					title="Add New Challange"
 					color="#A1EDCE"
