064893303f6bd9c02068fd7a2bbfcb62f618974e
Christina Liu <cyanliu@umich.edu>
2018-12-08 18:12:00 -0500

    now with less george

--- Home.js
+++ Home.js
@@ -65,7 +65,6 @@ export default class Home extends React.Component {
 						stylesContainer={styles.scroll}
 						horizontal={false}>
 
-					<Challenge name="george" />
 					{this.state.chalNames.map((chalName) => {
 						return (<Challenge name= {chalName} />)
 					})}
